24eda62a8de3c77496bb8bb5ad68d1d9ce604fff
Sumit Dhiman <ersumitdhiman@gmail.com>
2024-10-24 15:10:57 +0530

    url

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -30,7 +30,7 @@ export const authoptions = NextAuth({
     //   clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
     // }),
   ],
-  secret: process.env.NEXTAUTH_SECRET,
+  secret: process.env.secret,
   callbacks: {
     async signIn({ user, profile }) {
       await connectDB();
