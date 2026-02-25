31b27c32541c92e722f7078e575bbd87f9636d41
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-02-25 20:07:54 +0530

    console

--- app/[username]/page.js
+++ app/[username]/page.js
@@ -1,12 +1,10 @@
-"use client";
-/* eslint-disable react/no-unescaped-entities */ import React from "react";
+import React from "react";
 import PaymentPage from "@/components/PaymentPage";
 import { notFound } from "next/navigation";
 import { connectDB } from "@/lib/db";
-import User from "../models/User";
 
-const Username = ({ params }) => {
-  console.log("ENV:", process.env.MONGODB_URI);
+import User from "../models/User";
+const Username = async ({ params }) => {
   // if username is not present in database show, 404
   const checkUser = async () => {
     await connectDB();
@@ -16,7 +14,7 @@ const Username = ({ params }) => {
       return notFound();
     }
   };
-  checkUser();
+  await checkUser();
   const username = decodeURI(params.username);
   return <PaymentPage username={username} />;
 };
--- components/PaymentPage.js
+++ components/PaymentPage.js
@@ -36,39 +36,19 @@ const PaymentPage = ({ username }) => {
   };
 
   const pay = async (amount) => {
-    try {
-      const res = await fetch("/api/initiate", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          amount,
-          username,
-          paymentForm,
-        }),
-      });
+    const order = await initiate(amount, username, paymentForm);
 
-      if (!res.ok) {
-        console.error("API failed");
-        return;
-      }
+    const rzp = new Razorpay({
+      key: process.env.RAZORPAY_KEY_ID,
+      amount,
+      currency: "INR",
+      name: "GetMeAChai",
+      order_id: order.id,
+      callback_url: `${window.location.origin}/api/razorpay`,
+      theme: { color: "#00e0ff" },
+    });
 
-      const order = await res.json();
-
-      const rzp = new window.Razorpay({
-        key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
-        amount,
-        currency: "INR",
-        name: "GetMeAChai",
-        order_id: order.id,
-        callback_url: `${window.location.origin}/api/razorpay`,
-      });
-
-      rzp.open();
-    } catch (err) {
-      console.error("Payment error:", err);
-    }
+    rzp.open();
   };
 
   return (
