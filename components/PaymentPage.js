3631ccaae8b6a3269ba6a4498bac8e24ae1e5c2c
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-02-26 20:44:58 +0530

    change

--- components/PaymentPage.js
+++ components/PaymentPage.js
@@ -62,7 +62,7 @@ const PaymentPage = ({ username }) => {
       description: "Test Transaction",
       image: "https://example.com/your_logo",
       order_id: orderId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-      callback_url: `${location} / api / razorpay`,
+      callback_url: `${window.location.origin}/api/razorpay`,
       prefill: {
         //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
         name: "Gaurav Kumar", //your customer's name
