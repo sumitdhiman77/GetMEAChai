ea396dad4b06546b42905f502dc9dfef5615385a
Sumit Dhiman <ersumitdhiman@gmail.com>
2026-02-26 13:59:52 +0530

    change

--- components/PaymentPage.js
+++ components/PaymentPage.js
@@ -9,55 +9,89 @@ import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const PaymentPage = ({ username }) => {
-  const [paymentForm, setPaymentForm] = useState({});
+  // const { data: session } = useSession()
+
+  const [paymentForm, setPaymentForm] = useState({
+    name: "",
+    message: "",
+    amount: "",
+  });
   const [currentUser, setCurrentUser] = useState({});
   const [payments, setPayments] = useState([]);
+  const searchParams = useSearchParams();
   const router = useRouter();
-  const sp = useSearchParams();
 
   useEffect(() => {
-    loadData();
-    if (sp.get("paymentdone") === "true") {
-      toast.success("Thanks for the donation 🤍", {
-        autoClose: 1500,
-      });
-    }
-    router.push(`/${username}`);
+    getData();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const loadData = async () => {
-    const u = await fetchUser(username);
-    setCurrentUser(u);
-    const p = await fetchPayments(username);
-    setPayments(p);
-  };
+  useEffect(() => {
+    if (searchParams.get("paymentdone") == "true") {
+      toast("Thanks for your donation!", {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        transition: Bounce,
+      });
+    }
+    router.push(`/${username}`);
+  }, [searchParams, username, router]);
 
   const handleChange = (e) => {
     setPaymentForm({ ...paymentForm, [e.target.name]: e.target.value });
   };
 
+  const getData = async () => {
+    let u = await fetchUser(username);
+    setCurrentUser(u);
+    let dbPayments = await fetchPayments(username);
+    setPayments(dbPayments);
+  };
+
   const pay = async (amount) => {
-    const order = await initiate(amount, username, paymentForm);
-
-    const rzp = new Razorpay({
-      key: process.env.NEXT_PUBLIC_RAZORPAY_KEY_ID,
-      amount: amount,
+    // Get the order Id
+    let a = await initiate(amount, username, paymentForm);
+    let orderId = a.id;
+    var options = {
+      key: currentUser.razorpayid, // Enter the Key ID generated from the Dashboard
+      amount: amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
       currency: "INR",
-      name: "GetMeAChai",
-      order_id: order.id,
+      name: "Get Me A Chai", //your business name
+      description: "Test Transaction",
+      image: "https://example.com/your_logo",
+      order_id: orderId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
       callback_url: `${window.location.origin}/api/razorpay`,
-      theme: { color: "#00e0ff" },
-    });
+      prefill: {
+        //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
+        name: "Gaurav Kumar", //your customer's name
+        email: "gaurav.kumar@example.com",
+        contact: "9000090000", //Provide the customer's phone number for better conversion rates
+      },
+      notes: {
+        address: "Razorpay Corporate Office",
+      },
+      theme: {
+        color: "#3399cc",
+      },
+    };
 
-    rzp.open();
+    var rzp1 = new Razorpay(options);
+    rzp1.open();
   };
 
   return (
     <>
       <ToastContainer />
-      <Script src="https://checkout.razorpay.com/v1/checkout.js" />
-
+      <Script
+        src="https://checkout.razorpay.com/v1/checkout.js"
+        strategy="afterInteractive"
+      />
       <div className="bg-[#06080f] min-h-screen text-white pb-20">
         {/* FULL SCREEN COVER */}
         <div className="relative w-full h-72 md:h-[55vh]">
